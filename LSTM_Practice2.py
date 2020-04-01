84ba8b34e33ac4300aaea4773c5e106acd79b7e0
jehyun <ggurggur123@naver.com>
2020-04-01 15:41:48 +0900

    Update LSTM_Practice2.py

--- LSTM_Practice2.py
+++ LSTM_Practice2.py
@@ -1,19 +1,11 @@
 import pandas as pd
 import numpy as np
-%matplotlib inline
 import matplotlib.pyplot as plt
 df = pd.read_csv('C:/ML/mltoolkit-master/data/aptsellindex_gangnamgu.csv',parse_dates =["date"], index_col ="date") 
-#df.head()
-#df['trade_price_idx_value'].plot()
 
-#
 split_date = pd.Timestamp('01-01-2017')
 train = df.loc[:split_date, ['trade_price_idx_value']]
 test = df.loc[split_date:, ['trade_price_idx_value']]
-
-#ax = train.plot()
-#test.plot(ax=ax)
-#plt.legend(['train', 'test'])
 
 from sklearn.preprocessing import MinMaxScaler
 sc = MinMaxScaler()
@@ -22,7 +14,6 @@ test_sc = sc.transform(test)
 
 train_sc_df = pd.DataFrame(train_sc, columns=['trade_price_idx_value'], index=train.index)
 test_sc_df = pd.DataFrame(test_sc, columns=['trade_price_idx_value'], index=test.index)
-#train_sc_df.head()
 
 for s in range(1, 13):
     train_sc_df['shift_{}'.format(s)] = train_sc_df['trade_price_idx_value'].shift(s)
@@ -34,17 +25,12 @@ y_train = train_sc_df.dropna()[['trade_price_idx_value']]
 X_test = test_sc_df.dropna().drop('trade_price_idx_value', axis=1)
 y_test = test_sc_df.dropna()[['trade_price_idx_value']]
 
-#print(type(X_train))
 X_train = X_train.values
-#print(type(X_train))
 X_test= X_test.values
 
 y_train = y_train.values
 y_test = y_test.values
-#print(X_train.shape)
-#print(X_train)
-#print(y_train.shape)
-#print(y_train)
+
 X_train_t = X_train.reshape(X_train.shape[0], 12, 1)
 X_test_t = X_test.reshape(X_test.shape[0], 12, 1)
 
@@ -56,9 +42,9 @@ from keras.callbacks import EarlyStopping
 
 K.clear_session()
     
-model = Sequential() # Sequeatial Model 
+model = Sequential()
 model.add(LSTM(20, input_shape=(12, 1))) # (timestep, feature) 
-model.add(Dense(1)) # output = 1 
+model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam') 
 model.summary()
 early_stop = EarlyStopping(monitor='loss', patience=1, verbose=1)
