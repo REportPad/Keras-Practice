6aeafb8e0effe7a31ce6cb27dab0793c83b2884f
jehyun <ggurggur123@naver.com>
2020-04-02 16:27:13 +0900

    Update and rename Time series forecasting.py to Forecast a univariate time series.py

--- Forecast a univariate time series.py
+++ Forecast a univariate time series.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
@@ -76,12 +75,5 @@ EPOCHS = 10
 simple_lstm_model.fit(train_univariate, epochs=EPOCHS,
                       steps_per_epoch=EVALUATION_INTERVAL,
                       validation_data=val_univariate, validation_steps=50)
-                      
-                      
-for x, y in val_univariate.take(3):
-  plot = show_plot([x[0].numpy(), y[0].numpy(),
-                    simple_lstm_model.predict(x)[0]], 0, 'Simple LSTM model')
-  plot.show()
-
 
   
