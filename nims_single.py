a2a95bdd8f397a583a5da47846767ab76251261a
jehyun <ggurggur123@naver.com>
2020-04-03 17:53:12 +0900

    Create nims_single.py

---
+++ nims_single.py
@@ -0,0 +1,75 @@
+import tensorflow as tf
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+
+
+csv_path = 'C:/ML/city gas/train2.csv'
+
+#(Nx125)
+df = pd.read_csv(csv_path)
+
+#transpose
+df = df.T
+df = df.drop('id',0)#(125 x 78587)
+
+
+def univariate_data(dataset, start_index, end_index, history_size, target_size):
+    data = []
+    labels = []
+
+    start_index = start_index + history_size
+    if end_index is None:
+        end_index = len(dataset) - target_size
+
+    for i in range(start_index, end_index):
+        indices = range(i-history_size, i)
+        # Reshape data from (history_size,) to (history_size, 1)
+        data.append(np.reshape(dataset[indices], (history_size, 1)))
+        labels.append(dataset[i+target_size])
+    return np.array(data), np.array(labels)
+  
+TRAIN_SPLIT = 113
+tf.random.set_seed(13)
+  
+#Part 1: Forecast a univariate time series
+uni_data = df.iloc[:,0]
+uni_train_mean = uni_data[:TRAIN_SPLIT].mean()
+uni_train_std = uni_data[:TRAIN_SPLIT].std()
+uni_data = (uni_data-uni_train_mean)/uni_train_std
+
+univariate_past_history = 20
+univariate_future_target = 0
+uni_data = uni_data.to_numpy()
+x_train_uni, y_train_uni = univariate_data(uni_data, 0, TRAIN_SPLIT,
+                                           univariate_past_history,
+                                           univariate_future_target)
+x_val_uni, y_val_uni = univariate_data(uni_data, TRAIN_SPLIT, None,
+                                       univariate_past_history,
+                                       univariate_future_target)
+                                       
+BATCH_SIZE = 256
+BUFFER_SIZE = 10000
+
+train_univariate = tf.data.Dataset.from_tensor_slices((x_train_uni, y_train_uni))
+train_univariate = train_univariate.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+
+val_univariate = tf.data.Dataset.from_tensor_slices((x_val_uni, y_val_uni))
+val_univariate = val_univariate.batch(BATCH_SIZE).repeat()
+
+simple_lstm_model = tf.keras.models.Sequential([
+    tf.keras.layers.LSTM(8, input_shape=x_train_uni.shape[-2:]),
+    tf.keras.layers.Dense(1)
+])
+
+simple_lstm_model.compile(optimizer='adam', loss='mae')
+
+
+EVALUATION_INTERVAL = 200
+EPOCHS = 10
+
+simple_lstm_model.fit(train_univariate, epochs=EPOCHS,
+                      steps_per_epoch=EVALUATION_INTERVAL,
+                      validation_data=val_univariate, validation_steps=50)
